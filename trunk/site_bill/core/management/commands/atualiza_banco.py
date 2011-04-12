844ddfbe24171558e8a73eadd27ff4a1bdb0ab70
jhonstondalcin@gmail.com <jhonstondalcin@gmail.com@1040d22f-9442-fafe-1b8f-37cca1a39c86>
2011-04-12 00:36:21 +0000

    upgrade em models.py e atualiza_banco.py. login e cadastro de clientes funcionando. template RGB-Green adicionado em templates, mas ainda não aplicado ao base.

--- trunk/site_bill/core/management/commands/atualiza_banco.py
+++ trunk/site_bill/core/management/commands/atualiza_banco.py
@@ -15,18 +15,92 @@ class Command(NoArgsCommand):
 
     def handle_noargs(self, **options):
         
+        def cria_endereco(rua, num, complemento, bairro, cidade, estado, cep):
+            endereco = Endereco(rua = rua, num = num, complemento = complemento, bairro = bairro, cidade = cidade, estado = estado, cep = cep)
+            endereco.save()
+            return endereco
+        cliente1_endereco1 = cria_endereco('rua1 cliente1', '12341', 'complemento 11', 'bairro11', 'cidade11', 'estado11', '12312-123')
+        cliente1_endereco2 = cria_endereco('rua2 cliente1', '12342', 'complemento 12', 'bairro12', 'cidade12', 'estado12', '12312-123')
+        cliente2_endereco1 = cria_endereco('rua1 cliente2', '12343', 'complemento 2', 'bairro2', 'cidade2', 'estado2', '12312-123')
+        fornecedor1_endereco = cria_endereco('rua fornecedor1', '1234', 'complemento 1', 'bairro1', 'cidade1', 'estado1', '12312-123')
+        fornecedor2_endereco = cria_endereco('rua fornecedor2', '1234', 'complemento 2', 'bairro2', 'cidade2', 'estado2', '12312-123')
+        estoque1_endereco = cria_endereco('rua estoque1', '1234', 'complemento 1', 'bairro1', 'cidade1', 'estado1', '12312-123')
+        estoque2_endereco = cria_endereco('rua estoque2', '1234', 'complemento 2', 'bairro2', 'cidade2', 'estado2', '12312-123')
+        
+        
+        # CLIENTES
         def cria_usuario(username, email, password):
             user = User.objects.create_user(username, email, password)
             return user
-        
         user1 = cria_usuario('cliente1', 'jhonstondalcin@gmail.com', 'cliente1')
-        user2 = cria_usuario('cliente2', 'jhonstondalcin@gmail.com', 'cliente2')
+        user2 = cria_usuario('cliente2', 'marciohariki@gmail.com', 'cliente2')
         
-        
-        def cria_cliente(user, nome, email, email2, telefone, celular, rg, cpf, data_nascimento):
-            cliente = Cliente(user = user, email=email, email2=email2, nome = nome, telefone = telefone, rg = rg, cpf = cpf, data_nascimento = data_nascimento)
+        def cria_cliente(user, endereco, nome, email, email2, telefone, celular, rg, cpf, data_nascimento):
+            cliente = Cliente(user = user, endereco = endereco, email=email, email2=email2, nome = nome, telefone = telefone, rg = rg, cpf = cpf, data_nascimento = data_nascimento)
             cliente.save()
             return cliente
+        cliente1 = cria_cliente(user1, cliente1_endereco1, 'Cliente1', 'jhonstondalcin@gmail.com', 'jhonstondalcin@gmail.com', '7272-3060', '7272-3060', '46.705.983-4', '383.747.188-89', '1990-09-19')
+        cliente2 = cria_cliente(user2, cliente2_endereco1, 'Cliente2', 'marciohariki@gmail.com', 'marciohariki@gmail.com', '1234-5678', '1234-5678', '56.705.983-4', '483.747.188-89', '1991-09-19')
+        # /CLIENTES
         
-        cliente1 = cria_cliente(user1, 'Cliente1', 'jhonstondalcin@gmail.com', 'jhonstondalcin@gmail.com', '7272-3060', '7272-3060', '46.705.983-4', '383.747.188-89', '1990-09-19')
-        cliente2 = cria_cliente(user2, 'Cliente2', 'jhonstondalcin@gmail.com', 'jhonstondalcin@gmail.com', '1234-5678', '7272-3060', '46.705.983-4', '383.747.188-89', '1990-09-19')
+        # PRODUTOS
+        def cria_categoria_produto(categoria, descricao):
+            categoria_produto = CategoriaProduto(categoria = categoria, descricao = descricao)
+            categoria_produto.save()
+            return categoria_produto
+        categoria1_produto = cria_categoria_produto('Tênis', 'Os tênis da Bill World Sports são originais, eles combinam com você!')
+        categoria2_produto = cria_categoria_produto('Camisas de Times', 'As camisas de times da Bill World Sports são originais, eles combinam com você!')
+        
+        def cria_marca_produto(marca, descricao):
+            marca_produto = MarcaProduto(marca = marca, descricao = descricao)
+            marca_produto.save()
+            return marca_produto
+        marca1_produto = cria_marca_produto('Adidas', 'Adidas é uma empresa x, seus produtos são ótimos.')
+        marca2_produto = cria_marca_produto('Puma', 'Puma é uma empresa y, seus produtos são ótimos.')
+        
+        def cria_fornecedor(nome, descricao, contato, endereco):
+            fornecedor = Fornecedor(nome = nome, descricao = descricao, contato = contato, endereco = endereco)
+            fornecedor.save()
+            return fornecedor
+        fornecedor1 = cria_fornecedor('loja fornecedora1', 'descricao fornecedora 1', 'responsável pelo fornecedor: josé, tel 1111-1111', fornecedor1_endereco)
+        fornecedor2 = cria_fornecedor('loja fornecedora2', 'descricao fornecedora 2', 'responsável pelo fornecedor: joão, tel 1111-1111', fornecedor2_endereco)
+        
+        def cria_loja_estoque(nome, descricao, contato, endereco):
+            loja_estoque = LojaEstoque(nome = nome, descricao = descricao, contato = contato, endereco = endereco)
+            loja_estoque.save()
+            return loja_estoque
+        loja_estoque1 = cria_loja_estoque('loja estoque 1', 'descricao estoque 1', 'responsável pela loja estoque: josé, tel 1111-1111', estoque1_endereco)
+        loja_estoque2 = cria_loja_estoque('loja estoque 2', 'descricao estoque 2', 'responsável pela loja estoque: joão, tel 1111-1111', estoque2_endereco)
+        
+        def cria_especificacao_produto(tamanho, cor, tecido):
+            especificacao_produto = EspecificacaoProduto(tamanho = tamanho, cor = cor, tecido = tecido)
+            especificacao_produto.save()
+            return especificacao_produto
+        especificacao1_produto = cria_especificacao_produto('M', 'Azul/Marrom' , 'Couro')
+        especificacao2_produto = cria_especificacao_produto('P', 'Vermelho' , '100% Algodão')
+        
+        def cria_banco_produto(categoria, marca, fornecedor, loja_estoque, especificacao, nome, descricao, codigo, quantidade_estoque, valor_unitario_compra, valor_unitario_venda, data_compra_loja):
+            banco_produto = BancoProduto(categoria = categoria, marca = marca, fornecedor = fornecedor, loja_estoque = loja_estoque, especificacao = especificacao, nome = nome, descricao = descricao, codigo = codigo, quantidade_estoque = quantidade_estoque, valor_unitario_compra = valor_unitario_compra, valor_unitario_venda = valor_unitario_venda, data_compra_loja = data_compra_loja)
+            banco_produto.save()
+            return banco_produto
+        banco_produto1 = cria_banco_produto(categoria1_produto, marca1_produto, fornecedor1, loja_estoque1, especificacao1_produto, 'Tenis Adidas Modelo 2011', 'O mais novo modelo de tenis da adidas', '#0001T', '0', '70.55', '90.99', '2011-01-01')
+        banco_produto2 = cria_banco_produto(categoria2_produto, marca2_produto, fornecedor2, loja_estoque2, especificacao2_produto, 'Camisa Puma Modelo 2011', 'A mais nova camisa do time X', '#0001C', '5', '40.55', '70.99', '2011-01-01')
+        
+        # /PRODUTOS
+        
+        # OUTROS
+        def cria_texto(titulo, texto):
+            texto_pagina = TextoPagina(titulo = titulo, texto=texto)
+            texto_pagina.save()
+            return texto_pagina
+        texto_home = cria_texto('Titulo home', 'texto home')
+        texto_novidades = cria_texto('Titulo novidades', 'texto novidades')
+        
+        def cria_novidade(titulo, texto, link_produto):
+            novidade = Novidade(titulo = titulo, texto = texto, link_produto = link_produto)
+            novidade.save()
+            return novidade
+        novidade1 = cria_novidade('Chegaram os novos tenis da adidas', 'Você que estava esperando, chegaram hoje os tenis da Adidas! Corra pegar o seu!', 'link_produto_noticia1')
+        novidade2 = cria_novidade('Em breve camisetas do seu time!', 'Em breve camisetas do seu time! Dia XX/XX elas chegam na loja de Taguaí, não perca!', 'link_produto_noticia2')
+        
+        # /OUTROS
--- trunk/site_bill/sistema/models.py
+++ trunk/site_bill/sistema/models.py
@@ -2,80 +2,98 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+prefixo = settings.PREFIXO
 # Create your models here.
 
 # CLIENTE
 class Cliente(models.Model):
     user = models.ForeignKey(User, null=True, blank=True)
+    endereco = models.ForeignKey('Endereco', verbose_name="Endereço", null=True, blank=True)
     nome = models.CharField(max_length=128)
     email = models.EmailField(verbose_name="E-mail Principal")
     email2 = models.EmailField(verbose_name="E-mail Secundário",null=True, blank=True)
     data_nascimento = models.DateField(verbose_name="Data de nascimento", null=True, blank=True)
-    endereco = models.ForeignKey('EnderecoCliente', verbose_name="Endereço", null=True, blank=True)
     telefone = models.CharField(max_length=128, null=True, blank=True)
     celular = models.CharField(max_length=128, null=True, blank=True)
     rg = models.CharField(max_length=128,verbose_name="RG", null=True, blank=True)
-    cpf = models.CharField(max_length=128,verbose_name="CPF", null=True, blank=True)
+    cpf = models.CharField(max_length=128,verbose_name="CPF")
     
     def __unicode__(self):
         return self.nome
-    
-class EnderecoCliente(models.Model):
-    rua = models.CharField(max_length=128)
-    num = models.CharField(max_length=128, verbose_name="Número")
-    complemento = models.CharField(max_length=128, null=True, blank=True)
-    bairro = models.CharField(max_length=128)
-    cidade = models.CharField(max_length=128)
-    estado = models.CharField(max_length=128)
-    cep = models.CharField(max_length=128)
-    
-    def __unicode__(self):
-        return self.cep
     
 # /CLIENTE
 
 # PRODUTO
-class Produto(models.Model):
-    tipo = models.ForeignKey('TipoProduto')
+class BancoProduto(models.Model):
+    categoria = models.ForeignKey('CategoriaProduto')
+    marca = models.ForeignKey('MarcaProduto')
+    fornecedor = models.ForeignKey('Fornecedor', verbose_name="Fornecedor")
+    loja_estoque = models.ForeignKey('LojaEstoque', verbose_name="Loja que está estocado")
+    especificacao = models.OneToOneField('EspecificacaoProduto', verbose_name="Especificações do Produto")
     nome = models.CharField(max_length=128, verbose_name="Nome do Produto")
-    descricao = models.CharField(max_length=256, verbose_name="Descrição do Produto")
+    descricao = models.TextField(verbose_name="Descrição do Produto")
     codigo = models.CharField(max_length=128, verbose_name="Código do Produto")
-    quantidade_estoque = models.IntegerField()
+    quantidade_estoque = models.IntegerField(default = 0)
     valor_unitario_compra = models.FloatField(verbose_name="Valor Unitário de Compra")
     valor_unitario_venda = models.FloatField(verbose_name="Valor Unitário de Venda")
-    local_compra = models.ForeignKey('LocalCompra', verbose_name="Local da compra")
-    loja_estoque = models.ForeignKey('LojaEstoque', verbose_name="Loja que está estocado")
-    data_compra = models.DateField()
+    data_compra_loja = models.DateField(verbose_name="Quando o produto foi comprado e estocado?")
+    aparecer_banner = models.BooleanField(default= False, verbose_name="Mostrar no topo da Home?")
+    aparecer_vitrini = models.BooleanField(default= False, verbose_name="Mostrar na vitrini da Home?")
+    foto1 = models.FileField(upload_to=prefixo+'/static/fotos',null=True, default="default.png")
+    foto2 = models.FileField(upload_to=prefixo+'/static/fotos',null=True, default="default.png")
+    foto3 = models.FileField(upload_to=prefixo+'/static/fotos',null=True, default="default.png")
     
     def __unicode__(self):
         return self.nome
     
-class TipoProduto(models.Model):
-    tipo = models.CharField(max_length=128, verbose_name="Tipo")
-    descricao = models.CharField(max_length=256, verbose_name="Descrição")
+class CategoriaProduto(models.Model):
+    categoria = models.CharField(max_length=256, verbose_name="Categoria")
+    descricao = models.CharField(max_length=256, verbose_name="Descrição da Categoria")
     
     def __unicode__(self):
-        return self.tipo
+        return self.categoria
     
-class LocalCompra(models.Model):
-    nome = models.CharField(max_length=128, verbose_name="Nome do Local")
-    descricao = models.CharField(max_length=256, verbose_name="Descrição do Local")
-    contato = models.CharField(max_length=128, verbose_name="Contato do Local")
-    endereco = models.ForeignKey('EnderecoProduto', verbose_name="Endereço do Fornecedor", null=True, blank=True)
+class MarcaProduto(models.Model):
+    marca = models.CharField(max_length=256, verbose_name="Marca")
+    descricao = models.CharField(max_length=256, verbose_name="Descrição da Marca")
+    
+    def __unicode__(self):
+        return self.marca
+    
+class Fornecedor(models.Model):
+    endereco = models.ForeignKey('Endereco', verbose_name="Endereço do Fornecedor", null=True, blank=True)
+    nome = models.CharField(max_length=256, verbose_name="Nome do Fornecedor")
+    descricao = models.CharField(max_length=256, verbose_name="Descrição o Fornecedor")
+    contato = models.TextField(verbose_name="Contato Fornecedor")    
     
     def __unicode__(self):
         return self.nome
     
 class LojaEstoque(models.Model):
-    nome = models.CharField(max_length=128, verbose_name="Nome do Loja")
-    descricao = models.CharField(max_length=256, verbose_name="Descrição do Loja")
-    contato = models.CharField(max_length=128, verbose_name="Contato do Loja")
-    endereco = models.ForeignKey('EnderecoProduto', verbose_name="Endereço da Loja", null=True, blank=True)
+    endereco = models.ForeignKey('Endereco', verbose_name="Endereço da Loja", null=True, blank=True)
+    nome = models.CharField(max_length=128, verbose_name="Nome da Loja de Estoque/Venda")
+    descricao = models.CharField(max_length=256, verbose_name="Descrição da Loja")
+    contato = models.TextField(verbose_name="Contato da Loja")    
     
     def __unicode__(self):
         return self.nome
     
-class EnderecoProduto(models.Model):
+class EspecificacaoProduto(models.Model):
+    tamanho = models.CharField(max_length=256, null=True, blank=True, verbose_name="Tamanho")
+    cor = models.CharField(max_length=256, null=True, blank=True, verbose_name="Cor")
+    tecido = models.CharField(max_length=256, null=True, blank=True, verbose_name="Tipo de Tecido")
+    
+    def __unicode__(self):
+        return self.tamanho
+    
+# /PRODUTO
+
+#class Carrinho(models.Model):
+#    cliente = models.OneToOneField('Cliente')
+#    produtos = models.ForeignKey('BancoProduto')
+    
+class Endereco(models.Model):
     rua = models.CharField(max_length=128)
     num = models.CharField(max_length=128, verbose_name="Número")
     complemento = models.CharField(max_length=128, null=True, blank=True)
@@ -85,10 +103,9 @@ class EnderecoProduto(models.Model):
     cep = models.CharField(max_length=128)
     
     def __unicode__(self):
-        return self.cep
-    
-# /PRODUTO
-    
+        return self.cep    
+
+# PAGAMENTOS   
 #class Pagamento(models.Model):
 #    cliente = models.ForeignKey(Cliente)
 #    valor = models.FloatField()
@@ -101,3 +118,35 @@ class EnderecoProduto(models.Model):
 #    cliente = models.ForeignKey(Cliente)
 #    valor = models.FloatField()
 #    data_cobranca = models.DateField()
+# /PAGAMENTOS
+
+# OUTROS
+    
+class TextoPagina(models.Model):
+    titulo = models.CharField(max_length=256, verbose_name="Título da Página")
+    texto = models.TextField(verbose_name="Texto da Página")
+    
+class Novidade(models.Model):
+    titulo = models.CharField(max_length=256, verbose_name="Título da Novidade")
+    texto = models.TextField(verbose_name="Texto da Novidade")
+    foto = models.FileField(upload_to=prefixo+'/static/fotos',null=True, default="default.jpg")
+    link_produto = models.CharField(max_length=256, verbose_name="Link para o novo produto")
+    
+class Contato(models.Model):
+    
+    assunto_choice =(
+    ('duvida','Dúvida'),
+    ('sugestao','Sugestão'),
+    ('reclamacao','Reclamação'),
+    ('pedido','Pedido de Produto'),
+    ('conselheiro', 'Conselheiro'),
+    ('ex_membro', 'Ex-membro'),
+    )
+    
+    nome = models.CharField(max_length=128)
+    email = models.EmailField(verbose_name="E-mail")
+    telefone = models.CharField(max_length=128, null=True, blank=True)
+    texto = models.TextField(verbose_name="Comentário")
+    assunto = models.CharField(max_length=256, verbose_name="Assunto", choices=assunto_choice)
+        
+# /OUTROS
